e93e5faa6f82488e02564a9bab4dfa02fc7a5e53
Ray-Lindo <164272629+Ray-Lindo@users.noreply.github.com>
2025-06-03 18:37:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Função para detectar o click do mouse.
+
+// Função para detectar o pressionamento da tecla.
+
+// Função para alterar entre a teclas dos teclados para acionar o som
+
+// Função para animar os botões na hora de pressionar.
